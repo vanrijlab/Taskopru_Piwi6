41eaf6e368c9847191465de8017b9dec013f2051
Nynke van Eijk <nynke.vaneijk@radboudumc.nl>
2026-08-04 11:34:49 +0200

    Add files via upload

---
+++ 1_fastqc.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -l
+#$ -S /bin/bash
+#$ -cwd
+#$ -V
+#$ -N fastqc
+#$ -e ./fastqc.err.txt
+
+###############################################################################################################################################################################################
+# Author:          Nynke van Eijk
+# Contact:         nynke.vaneijk@radboudumc.nl
+# Adapted from:	   Zheng Y et al (2020). Protocol.io
+
+# Goal:            Quality control of FASTQ files
+# Input:           *.fastq.gz
+# Output:          *_fastqc.html
+#                  *_fastqc.zip
+# Requires:        fastqc v0.11.7 (Andrews, 2010) - http://www.bioinformatics.babraham.ac.uk/projects/fastqc
+###############################################################################################################################################################################################
+
+# Set-up
+projPath="<path/to/directory>"
+mkdir -p ${projPath}/fastq/fastQC
+
+# Run FastQC
+fastqc -q ${projPath}/fastq/*.fastq.gz -o ${projPath}/fastq/fastQC/
